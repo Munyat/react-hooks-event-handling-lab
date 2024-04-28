3bf61ddaa0627262c9cfc7f15f53ceff73d8b524
Brian Kipkirui Cheruiyot <briankipkiruimunyat@gmail.com>
2024-04-28 12:35:24 +0300

    event handling

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,14 @@
 // Code Keypad Component Here
-
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from "react";
+function Keypad() {
+  function change() {
+    console.log("Entering password...");
+  }
+  return (
+    <div>
+      <input type="password" onChange={change} />
+    </div>
+  );
 }
 
 export default Keypad;
